11f756787fdc6c55bd9681b2d5c281845f43a085
lidarmansiwon <kimsanmaro@naver.com>
2024-01-26 17:04:10 +0900

    change passThrough to crop box

--- launch/filter_launch.py
+++ launch/filter_launch.py
@@ -10,12 +10,17 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
             parameters=[
-                {"pass_through_z_min": -0.0},
-                {"pass_through_z_max": 0.5},
-                {"pass_through_x_min": -1.0},
-                {"pass_through_x_max": 1.0},
-                {"pass_through_y_min": -1.0},
-                {"pass_through_y_max": 1.0},
+                {"crop_box_x_min": -0.0},
+                {"crop_box_x_max": 10.5},
+                {"crop_box_y_min": -5.0},
+                {"crop_box_y_max": 5.0},
+                {"crop_box_z_min": -1.0},
+                {"crop_box_z_max": 1.0},
+                {"voxel_resolution": 0.2},
+                {"setMean": 10.0},
+                {"setStddevMulThresh": 0.5},
+                
+                
             ]
         )
     ])
--- src/pcl_filter.cpp
+++ src/pcl_filter.cpp
@@ -5,7 +5,7 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/filters/passthrough.h>
+#include <pcl/filters/crop_box.h>
 
 class VoxelGridAndOutlierRemovalNode : public rclcpp::Node
 {
@@ -29,57 +29,84 @@ public:
     }
 
 private:
-    double pass_through_z_min_;
-    double pass_through_z_max_;
-    double pass_through_x_min_;
-    double pass_through_x_max_;
-    double pass_through_y_min_;
-    double pass_through_y_max_;
+
+    double crop_box_z_min_;
+    double crop_box_z_max_;
+    double crop_box_x_min_;
+    double crop_box_x_max_;
+    double crop_box_y_min_;
+    double crop_box_y_max_;
+    double voxel_resolution_;
+    double setMean_;
+    double setStddevMulThresh_;
+    
 
     void declare_parameters()
-    {
-        declare_parameter<double>("pass_through_z_min", -1.0);
-        declare_parameter<double>("pass_through_z_max", 1.5);
-        declare_parameter<double>("pass_through_x_min", -10.0);
-        declare_parameter<double>("pass_through_x_max", 20.0);
-        declare_parameter<double>("pass_through_y_min", -10.0);
-        declare_parameter<double>("pass_through_y_max", 10.0);
+    {   
+        declare_parameter<double>("crop_box_y_min", -10.0);
+        declare_parameter<double>("crop_box_y_max", 10.0);
+        declare_parameter<double>("crop_box_z_min", -1.0);
+        declare_parameter<double>("crop_box_z_max", 1.5);
+        declare_parameter<double>("crop_box_x_min", -10.0);
+        declare_parameter<double>("crop_box_x_max", 20.0);
+
+        declare_parameter<double>("voxel_resolution", 0.5);
+        declare_parameter<double>("setMean", 10.0);
+        declare_parameter<double>("setStddevMulThresh", 1.0);
     }
 
     void get_parameters()
+    {   
+        get_parameter("crop_box_y_min", crop_box_y_min_);
+        get_parameter("crop_box_y_max", crop_box_y_max_);
+        get_parameter("crop_box_z_min", crop_box_z_min_);
+        get_parameter("crop_box_z_max", crop_box_z_max_);
+        get_parameter("crop_box_x_min", crop_box_x_min_);
+        get_parameter("crop_box_x_max", crop_box_x_max_);
+
+        get_parameter("voxel_resolution", voxel_resolution_);
+        get_parameter("setMean", setMean_);
+        get_parameter("setStddevMulThresh", setStddevMulThresh_);
+    }
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr applyCropBoxFilter(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud)
     {
-        get_parameter("pass_through_z_min", pass_through_z_min_);
-        get_parameter("pass_through_z_max", pass_through_z_max_);
-        get_parameter("pass_through_x_min", pass_through_x_min_);
-        get_parameter("pass_through_x_max", pass_through_x_max_);
-        get_parameter("pass_through_y_min", pass_through_y_min_);
-        get_parameter("pass_through_y_max", pass_through_y_max_);
+        pcl::CropBox<pcl::PointXYZI> crop_box;
+        crop_box.setInputCloud(input_cloud);
+        crop_box.setMin(Eigen::Vector4f(crop_box_x_min_, crop_box_y_min_, crop_box_z_min_, 1.0));
+        crop_box.setMax(Eigen::Vector4f(crop_box_x_max_, crop_box_y_max_, crop_box_z_max_, 1.0));
+
+        pcl::PointCloud<pcl::PointXYZI>::Ptr output_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        crop_box.filter(*output_cloud);
+
+        return output_cloud;
+    }
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr convertPointCloud2ToPCL(const sensor_msgs::msg::PointCloud2::SharedPtr input)
+    {
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::fromROSMsg(*input, *cloud);
+    return cloud;
     }
 
     void lidar_callback(const sensor_msgs::msg::PointCloud2::SharedPtr input)
     {
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
         pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_downsampled(new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cutted(new pcl::PointCloud<pcl::PointXYZI>);
         pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
 
-        pcl::fromROSMsg(*input, *cloud);
+        // pcl::fromROSMsg(*input, *cloud);
 
-        // PassThrough filter
-        pcl::PassThrough<pcl::PointXYZI> cut_points;
-        cut_points.setInputCloud (cloud);                                           //입력 
-        cut_points.setFilterFieldName("z");                                         //적용할 좌표 축 (eg. Z축)
-        cut_points.setFilterLimits(pass_through_z_min_, pass_through_z_max_);       //적용할 값 (최소, 최대 값)  
-        cut_points.setFilterFieldName("x");                                             
-        cut_points.setFilterLimits(pass_through_x_min_, pass_through_x_max_);
-        cut_points.setFilterFieldName("y");
-        cut_points.setFilterLimits(pass_through_y_min_, pass_through_y_max_);
-        cut_points.filter(*cloud_cutted);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = convertPointCloud2ToPCL(input);
+
+        // CropBox filter
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cropped = applyCropBoxFilter(cloud);
+
 
         // VoxelGrid downsampling
         pcl::VoxelGrid<pcl::PointXYZI> sor_downsample;
-        sor_downsample.setInputCloud(cloud_cutted);
-        sor_downsample.setLeafSize(0.5, 0.5, 0.5); // Set the voxel size
+        sor_downsample.setInputCloud(cloud_cropped);
+        sor_downsample.setLeafSize(voxel_resolution_, voxel_resolution_, voxel_resolution_); // Set the voxel size
         sor_downsample.filter(*cloud_downsampled);
         // Publish the Data
         sensor_msgs::msg::PointCloud2 downsampled;
@@ -91,8 +118,8 @@ private:
         // StatisticalOutlierRemoval filtering
         pcl::StatisticalOutlierRemoval<pcl::PointXYZI> sor_filter;
         sor_filter.setInputCloud(cloud_downsampled);
-        sor_filter.setMeanK(10);
-        sor_filter.setStddevMulThresh(1);
+        sor_filter.setMeanK(setMean_);
+        sor_filter.setStddevMulThresh(setStddevMulThresh_);
         sor_filter.filter(*cloud_filtered);
 
         // Publish the Data
